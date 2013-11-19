a599def1064ebfe725c2965b6e2bd3cfc257b12d
Sergi Reñé <sergi.rene@entel.upc.edu>
2013-11-19 14:58:28 +0100

    Initial version

--- build/build-status.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-
-# Programs that are runnable.
-ns3_runnable_programs = ['ns3.15-scratch-simulator-debug', 'ns3.15-subdir-debug', 'ns3.15-tcp-bulk-p2p-debug']
-
-# Scripts that are runnable.
-ns3_runnable_scripts = []
-
